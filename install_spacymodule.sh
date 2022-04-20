d5d33ab370c8708458de935ee36e347c2943bc98
dionis <d.lopez@alltech.fr>
2022-04-20 14:31:15 -0400

    Update python name in Spacy module install

--- install_spacymodule.sh
+++ install_spacymodule.sh
@@ -5,8 +5,8 @@ cd opinion_process/nlp
 mkdir pretrained
 mkdir pretrained/bert
 cd ../linguisticrule
-python -m spacy download en_core_web_sm
-python -m spacy download es_core_web_sm
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download es_core_web_sm
 cd ../../
 #TEST DIRECTORY
 echo "******************** TEST DIRECTORY *******************"
@@ -15,12 +15,12 @@ mkdir pretrained
 mkdir pretrained/bert
 mkdir spacy_module
 cd spacy_module
-python -m spacy download en_core_web_sm
-python -m spacy download es_core_web_sm
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download es_core_web_sm
 cd ../../
 #ROOT DIRECTORY
 echo "********************ROOT DIRECTORY*******************"
 mkdir spacy_module
 cd spacy_module
-python -m spacy download en_core_web_sm
-python -m spacy download es_core_web_s
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download es_core_web_s

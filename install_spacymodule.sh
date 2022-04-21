6f1697a292f6d51a800d865ba696d93c55e04bbb
dionis <d.lopez@alltech.fr>
2022-04-20 20:12:18 -0400

    Fix Spacy english and spain module

--- install_spacymodule.sh
+++ install_spacymodule.sh
@@ -5,8 +5,8 @@ cd opinion_process/nlp
 mkdir pretrained
 mkdir pretrained/bert
 cd ../linguisticrule
-python3 -m spacy download en_core_web_sm
-python3 -m spacy download es_core_web_sm
+python3 -m spacy download en_core_web_md
+python3 -m spacy download es_core_news_sm
 cd ../../
 #TEST DIRECTORY
 echo "******************** TEST DIRECTORY *******************"
@@ -15,12 +15,18 @@ mkdir pretrained
 mkdir pretrained/bert
 mkdir spacy_module
 cd spacy_module
-python3 -m spacy download en_core_web_sm
-python3 -m spacy download es_core_web_sm
+python3 -m spacy download en_core_web_md
+python3 -m spacy download es_core_news_sm
 cd ../../
 #ROOT DIRECTORY
 echo "********************ROOT DIRECTORY*******************"
 mkdir spacy_module
 cd spacy_module
-python3 -m spacy download en_core_web_sm
-python3 -m spacy download es_core_web_s
+python3 -m spacy download en_core_web_md
+python3 -m spacy download es_core_news_sm
+
+#
+#python -m venv .env
+#source .env/bin/activate
+#pip install -U pip setuptools wheel
+#pip install -U spacy
